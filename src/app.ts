32f94127cd367e3f2eac858793d1ad9fff4dc15a
rafioul-hasan-58 <rafioulhasan2@.com>
2026-04-23 15:54:45 +0600

    fix: new cors origin added

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
-app.use(cors({ origin: ['http://localhost:9999', 'https://dima360ai.com'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:9999', 'https://dima360ai.com', "https://www.dima360marketing.com", "https://www.dima360ai.com"], credentials: true }));
 
 // app routes
 app.use('/api/v1', router);
